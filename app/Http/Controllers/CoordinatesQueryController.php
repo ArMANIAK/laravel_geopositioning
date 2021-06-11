f5f61d8b59746729123377cfab912d3dce3b6589
Mikhail Aloshyn <mikhail.alyoshyn@gmail.com>
2021-06-11 09:59:43 +0300

    attempt: tried to avoid saving same requests

--- app/Http/Controllers/CoordinatesQueryController.php
+++ app/Http/Controllers/CoordinatesQueryController.php
@@ -16,35 +16,41 @@ class CoordinatesQueryController extends Controller
         $latitude = $request->input('latitude');
         $longitude = $request->input('longitude');
 
-        $requestedCoords = CoordinatesQuery::create([
+        $requestedCoords = CoordinatesQuery::firstOrNew([
             'longitude' => $longitude,
             'latitude' => $latitude
         ]);
-        $uri = '/geocoding/v5/mapbox.places/' . $longitude . ',' . $latitude . '.json?types=poi&access_token=' . 
-            getenv('GEO_API_TOKEN');
-        $response = Http::get(getenv('GEO_API') . $uri);
-        if (empty($response->json()['features']))
+        if ($requestedCoords->response_id)
         {
-            echo 'Nothing is here';
-            $fullAddress = $city['text'] = $region['text'] = $country['text'] = 'Undefined';
+            $responseAddress = Response::find($requestedCoords->response_id);
+            echo "$responseAddress->address<br/>Country: {$responseAddress->country}, State: {$responseAddress->region}, City: {$responseAddress->city}<br/>";
         } else {
-            $fullAddress = $response->json()['features'][0]['place_name'];
-            $address_array = $response->json()['features'][0]['context'];
-            [$city, $region, $country] = array_slice($address_array, -3);
-            echo "$fullAddress<br/>Country: {$country['text']}, State: {$region['text']}, City: {$city['text']}<br/>";
+            $uri = '/geocoding/v5/mapbox.places/' . $longitude . ',' . $latitude . '.json?types=poi&access_token=' . 
+                getenv('GEO_API_TOKEN');
+            $response = Http::get(getenv('GEO_API') . $uri);
+            if (empty($response->json()['features']))
+            {
+                echo 'Nothing is here';
+                $fullAddress = $city['text'] = $region['text'] = $country['text'] = 'Undefined';
+            } else {
+                $fullAddress = $response->json()['features'][0]['place_name'];
+                $address_array = $response->json()['features'][0]['context'];
+                [$city, $region, $country] = array_slice($address_array, -3);
+                echo "$fullAddress<br/>Country: {$country['text']}, State: {$region['text']}, City: {$city['text']}<br/>";
+            }
+            $requestedRegion = Region::firstOrNew([
+                'country' => $country['text'],
+                'region' => $region['text'],
+                'city' => $city['text']
+            ]);
+            $requestedRegion->save();
+            $responseAddress = Response::firstOrNew([
+                'region_id' => $requestedRegion->id,
+                'address' => $fullAddress
+            ]);
+            $requestedCoords->response_id = $responseAddress->id;
+            $responseAddress->save();
+            $requestedCoords->save();
         }
-        $requestedRegion = Region::firstOrNew([
-            'country' => $country['text'],
-            'region' => $region['text'],
-            'city' => $city['text']
-        ]);
-        $requestedRegion->save();
-        $responseAddress = Response::firstOrNew([
-            'region_id' => $requestedRegion->id,
-            'address' => $fullAddress
-        ]);
-        $requestedCoords->response_id = $responseAddress->id;
-        $requestedCoords->save();
-        $responseAddress->save();
     }
 }
